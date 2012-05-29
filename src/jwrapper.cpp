6ca77c84381e2e97b03c08f102dab45a2032e899
Wangxr <vintage.wang@gmail.com>
2012-05-29 22:49:40 +0800

    Remove boost

--- src/jwrapper.cpp
+++ src/jwrapper.cpp
@@ -1,8 +1,18 @@
 #include "stdafx.h"
 
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+using namespace boost::property_tree;
+
+void test_ptree()
+{
+
+}
+
 int main(int argc, char** argv)
 {
 	printf("Hello World.\n");
+	test_ptree();
 	return 0;
 }
 
