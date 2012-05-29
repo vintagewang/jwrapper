9a1c24c4062caaf52ba49e385f4900b8f802da78
Wangxr <vintage.wang@gmail.com>
2012-05-29 23:21:53 +0800

    Boost ptree OK

--- src/jwrapper.cpp
+++ src/jwrapper.cpp
@@ -1,12 +1,28 @@
 #include "stdafx.h"
-
+#include <iostream>
+#include <string>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
+#include <boost/typeof/std/utility.hpp>
 using namespace boost::property_tree;
+using std::cout;
+using std::endl;
+using std::string;
 
 void test_ptree()
 {
+	ptree pt;
+	read_xml("jwrapper.xml", pt);
+	cout << pt.get<string>("java.javahome") << endl;
+	cout << pt.get<string>("java.mainclass") << endl;
 
+	BOOST_AUTO(child, pt.get_child("java.classpaths"));
+	for (BOOST_AUTO(pos, child.begin());
+		pos != child.end();
+		++pos)
+	{
+		cout << pos->second.data()<< ",";
+	}
 }
 
 int main(int argc, char** argv)
