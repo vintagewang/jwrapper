0a7c9deeb52d825b6c54964a446652655833c11d
Wangxr <vintage.wang@gmail.com>
2012-06-10 19:57:19 +0800

    add java hello world example

---
+++ samples/Hello/src/main/java/com/github/vintage/wang/jwrapper/sample/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.github.vintage.wang.jwrapper.sample;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.exit(0);
+	}
+}
