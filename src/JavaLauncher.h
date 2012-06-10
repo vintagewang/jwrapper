244700201890fe56162f3c30477e45fc68e71ad8
Wangxr <vintage.wang@gmail.com>
2012-06-10 14:24:03 +0800

    add JavaLauncher class

---
+++ src/JavaLauncher.h
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2012 Wangxr, vintage.wang@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef _JW_JAVA_LAUNCHER_H__
+#define _JW_JAVA_LAUNCHER_H__
+#include <jni.h>
+#include <string>
+
+class JavaLauncher
+{
+public:
+
+	JavaLauncher();
+	~JavaLauncher();
+	static JavaLauncher* getInstance();
+
+	bool launchJavaApp(int argc, char** argv);
+	static void exitHandler(int code);
+	static void abortHandler(int code);
+
+private:
+};
+
+#endif // end of _JW_JAVA_LAUNCHER_H__
